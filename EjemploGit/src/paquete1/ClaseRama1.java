02d97594037858a45f082ab725b80a9d07cf14fc
jsi17 <jsi17@Javi_Trabajo>
2025-03-19 19:40:38 +0100

    S

---
+++ EjemploGit/src/paquete1/ClaseRama1.java
@@ -0,0 +1,7 @@
+package paquete1;
+
+public class ClaseRama1 {
+	public static void main(String[] args) {
+		System.out.println("Clase en Rama 1");
+	}
+}
--- EjemploGit/src/pruebas/Prueba.java
+++ EjemploGit/src/pruebas/Prueba.java
@@ -2,6 +2,6 @@ package pruebas;
 
 public class Prueba {
 	public static void main(String[] args) {
-		System.out.println("Version 4");
+		System.out.println("Rama Master");
 	} 
 }
